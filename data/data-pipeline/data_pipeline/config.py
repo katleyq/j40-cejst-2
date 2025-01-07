d4898b8f5539ed6c6497c4caa00f00fe7ffb2fce
Carlos Felix <63804190+carlosfelix2@users.noreply.github.com>
2025-01-07 14:18:58 -0500

    Improve download retry logic

--- data/data-pipeline/data_pipeline/config.py
+++ data/data-pipeline/data_pipeline/config.py
@@ -12,7 +12,8 @@ settings = Dynaconf(
 # set root dir
 settings.APP_ROOT = pathlib.Path(data_pipeline.__file__).resolve().parent
 settings.DATA_PATH = settings.APP_ROOT / "data"
-settings.REQUESTS_DEFAULT_TIMOUT = 3600
+settings.REQUESTS_DEFAULT_TIMOUT = 300
+settings.REQUESTS_DEFAULT_RETRIES = 3
 # To set an environment use:
 # Linux/OSX: export ENV_FOR_DYNACONF=staging
 # Windows: set ENV_FOR_DYNACONF=staging
