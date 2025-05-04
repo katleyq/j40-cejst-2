6cfa9fe6d41cb10bd250e5e2cc0103449aa19dde
haylee360 <hayleeoyler360@gmail.com>
2025-05-04 14:18:39 -0700

    Edit contact page for plotting

--- client/src/components/ObservableTest/ObservableTest.tsx
+++ client/src/components/ObservableTest/ObservableTest.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import * as Plot from "@observablehq/plot";
+// import React from "react";
+// import * as Plot from "@observablehq/plot";
 
-const ObservableTest = () => {
-  const data = [
-    { category: "A", value: 10 },
-    { category: "B", value: 20 },
-    { category: "C", value: 15 },
-  ];
+// const ObservableTest = () => {
+//   const data = [
+//     { category: "A", value: 10 },
+//     { category: "B", value: 20 },
+//     { category: "C", value: 15 },
+//   ];
 
-  const chart = Plot.plot({
-    marks: [Plot.barY(data, { x: "category", y: "value" })],
-  });
+//   const chart = Plot.plot({
+//     marks: [Plot.barY(data, { x: "category", y: "value" })],
+//   });
 
-  React.useEffect(() => {
-    document.body.appendChild(chart);
-    return () => chart.remove();
-  }, []);
+//   React.useEffect(() => {
+//     document.body.appendChild(chart);
+//     return () => chart.remove();
+//   }, []);
 
-  return <div />;
-};
+//   return <div />;
+// };
 
-export default ObservableTest;
+// export default ObservableTest;
