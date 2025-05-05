234199c548fe689283c32b3e13a33e8df11bfbc8
haylee360 <hayleeoyler360@gmail.com>
2025-05-05 09:29:09 -0700

    Add contact page back, still working on importing observable

---
+++ client/src/pages/contact.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import {Grid} from '@trussworks/react-uswds';
+// import * as Plot from '@observablehq/plot';
+
+import J40MainGridContainer from '../components/J40MainGridContainer';
+import Layout from '../components/layout';
+import ObservableTest from '../components/ObservableTest';
+
+interface IContactPageProps {
+  location: Location;
+}
+
+const ContactPage = ({location}: IContactPageProps) => {
+  return (
+    <Layout location={location} title={'Observable'}>
+      <J40MainGridContainer>
+        <section className={'page-heading'}>
+          <h1>Testing Observable Plots</h1>
+        </section>
+        <ObservableTest></ObservableTest>
+
+        <Grid row gap={6}>
+          {/* First column */}
+          <Grid desktop={{col: 8}} col={12}>
+            <h2>Woohoo</h2>
+          </Grid>
+
+          {/* Second Column */}
+          <Grid desktop={{col: 4}} col={12}></Grid>
+        </Grid>
+      </J40MainGridContainer>
+    </Layout>
+  );
+};
+
+export default ContactPage;
