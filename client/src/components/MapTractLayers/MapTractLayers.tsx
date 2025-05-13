31e2a5fc815e116bc26c1fe360e4cfc06938acbb
haylee360 <hayleeoyler360@gmail.com>
2025-05-13 18:35:09 +0000

    Revert "Merge pull request #12 from haylee360/main"
    This reverts commit f563ce9df2e93951a960eec3ffeb6778d0218242, reversing
    changes made to 35acad2bdceffdff3717386f9d413fd103e50a5b.

--- client/src/components/MapTractLayers/MapTractLayers.tsx
+++ client/src/components/MapTractLayers/MapTractLayers.tsx
@@ -505,11 +505,6 @@ const MapTractLayers = ({
             <Layer
               id={constants.ADD_BURDEN_HIGH_LAYER_ID}
               source-layer={constants.SCORE_SOURCE_LAYER}
-              filter={[
-                '>',
-                constants.TOTAL_POPULATION,
-                constants.PSIM_POP_THRESHOLD,
-              ]}
               type="fill"
               paint={{
                 'fill-color': [
@@ -624,11 +619,6 @@ const MapTractLayers = ({
             <Layer
               id={constants.ADD_INDICATOR_HIGH_LAYER_ID}
               source-layer={constants.SCORE_SOURCE_LAYER}
-              filter={[
-                '>',
-                constants.TOTAL_POPULATION,
-                constants.PSIM_POP_THRESHOLD,
-              ]}
               type="fill"
               paint={{
                 'fill-color': [
